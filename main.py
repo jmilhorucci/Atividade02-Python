0833197e3f680773e10e2c2413ef44304427d56b
jmilhorucci <44588590+jmilhorucci@users.noreply.github.com>
2021-08-20 22:42:54 -0300

    tividade 02

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+import json
+r =requests.get('https://restcountries.eu/rest/v2/lang/it')
+#print(r.text)
+js = json.loads(r.text)
+#print(len(js[0]["topLevelDomain"]))
+
+class Country:
+  def __init__(self, name, callingCodes, capital, population, area):
+
+    self.name = name
+    self.callingCodes = callingCodes
+    self.capital = capital
+    self.population = population
+    self.area = area
+
+  def __str__(self):
+    return f"A(name:{self.name}, callingCodes:{self.callingCodes}, capital:{self.capital}, population:{self.population}, area:{self.area})"
+
+CountryA = Country (
+  name = (js[1]["name"]),
+  callingCodes = (js[1]["callingCodes"]),
+  capital = (js[1]["capital"]),
+  population = int(js[1]["population"]),
+  area = float(js[1]["population"])
+)
+
+print(CountryA)
